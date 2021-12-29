3028845b985d80b634731b7c3d91b804b3b91a98
Roy Avigdorov <royavig@post.bgu.ac.il>
2021-12-29 21:38:45 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, redirect, render_template,session,request,url_for
+
+app = Flask(__name__)
+
+@app.route('/home_page')
+@app.route('/home')
+@app.route('/')
+def home_func():
+    return render_template('Cv.html')
+
+
+@app.route('/movies')
+def tv():
+    return render_template('movies.html')
+
+
+@app.route('/assignment8')
+def assigment8():
+    return render_template('Assignment8.html',
+                           user={'firstname': "Roy", 'lastname': "Avigdorov"},
+                           hobbies=['movies', 'Hockey', 'Tennis'])
+
+
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9():
+    username = ''
+    users = [{'id': 1, 'email': "roy.a@gmail.com", 'firstname': "roy", 'lastname': "avigdorov"},
+        {'id': 2, 'email': "noam.Sul@gmail.com", 'firstname': "noam", 'lastname': "shulanski"},
+        {'id': 3, 'email': "yoni.amid@gmail.com", 'firstname': "yoni", 'lastname': "amid"},
+        {'id': 4, 'email': "tal.sorek@gmail.com", 'firstname': "tal", 'lastname': "sorek"},
+        {'id': 5, 'email': "tom.kara@gmail.com", 'firstname': "tom", 'lastname': "kara"}]
+    firstname = ''
+    logged_in = True
+
+    if request.method == 'GET':
+        if 'firstname' in request.args:
+            firstname = request.args['firstname']
+
+    if request.method == 'POST':
+        username = request.form['username']
+        session['logged_in'] = True
+        session['username'] = username
+    return render_template('Assignment9.html',
+                           request_method=request.method,
+                           name=firstname,
+                           users=users,
+                           username=username)
+
+
+@app.route('/log_out')
+def log_out():
+    session.pop('username')
+    session['logged_in'] = False
+    session['username'] = ''
+    return redirect('/assignment9')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/js.js
@@ -0,0 +1,7 @@
+function validEmail(EmailC, note1){
+    const inpObj= document.getElementById("EmailC");
+    if(!inpObg.checkValidity()){
+        document.getElementById(note1).innerHTML= inpObj.validationMwssage;
+    }
+}
+
